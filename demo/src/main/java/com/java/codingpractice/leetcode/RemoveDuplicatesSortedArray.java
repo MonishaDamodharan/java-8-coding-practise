bcadb52bb0d4f355caf19a7c5418904c035e9017
Monisha <missmonishavimala@gmail.com>
2026-07-17 15:53:16 +0530

    Remove duplicates example code

---
+++ demo/src/main/java/com/java/codingpractice/leetcode/RemoveDuplicatesSortedArray.java
@@ -0,0 +1,40 @@
+package com.java.codingpractice.leetcode;
+
+import java.util.Scanner;
+
+public class RemoveDuplicatesSortedArray {
+
+	public static void main(String[] args) {
+		// 26. Remove Duplicates from sorted array
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Enter values : ");
+		
+
+	}
+	
+//	public int removeDuplicates(int[] nums) {
+//        int numbers[] = [];
+//        int j=0;
+//        for(int i=0; i<= nums.length; i++){
+//            int temp = 0;
+//            if(i=0){
+//                temp = nums[i];
+//                numbers.add(temp);
+//                j++;
+//            }else{
+//                if(temp = nums[i]){
+//                    numbers.add("_");
+//                }else{
+//                    temp = nums[i];
+//                    numbers.add(temp);
+//                    j++;
+//                }
+//            }
+//
+//        }
+//        return j;
+//
+//        
+//    }
+
+}
